debaec1774e504b5ded15c33ccb50c9c1a381175
Wenlao Peng <s3690691@student.rmit.edu.au>
2023-11-08 12:21:50 +1100

    Lifting state up

---
+++ lib/questions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QuesionsScreen extends StatefulWidget {
+  const QuesionsScreen({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _QuestionsScreen();
+  }
+}
+
+class _QuestionsScreen extends State<QuesionsScreen> {
+  @override
+  Widget build(context) {
+    return const Text('questions screen');
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuesionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 94, 21, 221),
+                Color.fromARGB(255, 67, 25, 139),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
