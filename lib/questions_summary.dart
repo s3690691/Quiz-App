d67e5078f5ece6dbaf147760619551e3cce1518a
Wenlao Peng <s3690691@student.rmit.edu.au>
2023-11-29 16:11:06 +0800

    Update question summary map

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryDate, {super.key});
+  final List<Map<String, Object>> summaryDate;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      children: summaryDate.map((data) {
+        return Row(
+          children: [
+            Text(((data['question_index'] as int) + 1).toString()),
+          ],
+        );
+      }).toList(),
+    );
   }
 }
